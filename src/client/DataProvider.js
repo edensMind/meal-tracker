f80c416c5484707a74092bf4d740dee1fe16e4a3
edensMind <edenmind@gmail.com>
2020-12-27 21:08:45 -0500

    data provider

--- src/client/DataProvider.js
+++ src/client/DataProvider.js
@@ -1,16 +1,18 @@
 import axios from 'axios';
 
-const GetUser = axios.get(`/api/getUsername`);
-
-const GetAllFood = axios.get(`/api/food`);
-
-const GetFoodById = (id) => axios.get(`/api/food/${id}`);
-
-const GetMealsForTime = (startDate, endDate) => {
-    // /api/meal/history/2020-12-26/2020-12-27
-    return axios.get(`/api/meal/history/${startDate}/${endDate}`);
+const headers = {
+    headers: {
+        'content-type': 'application/json'
+    }
 };
 
+// DB Calls GETs
+const GetUser = () => axios.get(`/api/getUsername`);
+const GetAllFood = () => axios.get(`/api/food`);
+const GetFoodById = (id) => axios.get(`/api/food/${id}`);
+const GetMealsForTime = (startDate, endDate) => axios.get(`/api/meal/history/${startDate}/${endDate}`);
+
+// DB Calls POSTs
 const CreateNewFood = (foodData) => {
     foodData = {
         "description": "Apples",
@@ -18,17 +20,48 @@ const CreateNewFood = (foodData) => {
         "servingSize": "1 cup",
         "image": "apples.png",
     };
-    return axios.post(`/api/food`, foodData,{
-        headers: {
-            'content-type': 'application/json'
-        }
-    });
+    return axios.post(`/api/food`, foodData, headers);
+};
+const CreateNewMeal = (foodData) => {
+    foodData = {
+        "food": []
+    };
+    return axios.post(`/api/meal`, foodData, headers);
 };
 
+// DB Calls PUTs
+const AddFoodToMeal = (id, foodData) => {
+    foodData = {
+        "food": []
+    };
+    return axios.put(`/api/meal/${id}`, foodData, headers);
+};
+const EditFoodItem = (id, foodData) => {
+    foodData = {
+        "description": "Apples",
+        "calories": 50,
+        "servingSize": "1 cup",
+        "image": "apples.png",
+    };
+    return axios.put(`/api/food/${id}`, foodData, headers);
+};
+
+// DB Calls DELETEs
+const RemoveFoodItem = (id) => axios.delete(`/api/food/${id}`);
+const RemoveFoodFromMeal = (mealId, foodId) => axios.delete(`/api/mealFood/${mealId}/${foodId}`);
+const RemoveMeal = (id) => axios.delete(`/api/meal/${id}`);
+
+//Exports
 export { 
     GetUser,
     GetAllFood,
     GetFoodById,
     GetMealsForTime,
     CreateNewFood,
+    CreateNewMeal,
+    AddFoodToMeal,
+    EditFoodItem,
+    RemoveFoodItem,
+    RemoveFoodFromMeal,
+    RemoveMeal,
 };
